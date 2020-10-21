e6a125e6886a6b1229484d2adfb48470d512815b
Bill Fisher <william.w.fisher@gmail.com>
2020-10-21 16:52:29 -0700

    Check for exacl binary.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,14 @@
 
 # Run all test suites using shunit2.
 
+script_dir=`dirname "$0"`
+cd "$script_dir"
+
+if [ ! -f ../target/debug/exacl ]; then
+    echo "exacl executable not found!"
+    exit 1
+fi
+
 for t in testsuite_*.sh ; do 
     shunit2 $t
 done
