e4d9c0a32fa0550ecc4752b5885c1edc2f191a62
Bill Fisher <william.w.fisher@gmail.com>
2020-10-21 19:36:32 -0700

    Fix code coverage script.

--- ci/code_coverage.sh
+++ ci/code_coverage.sh
@@ -14,17 +14,17 @@ export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Co
 export RUSTDOCFLAGS="-Cpanic=abort"
 
 # Build & Test
-cargo install grcov
-cargo +nightly build
+cargo +nightly install grcov
 cargo +nightly test
+cargo +nightly build
 ./tests/run_tests.sh
 
 if [ $arg1 = "open" ]; then
-    # Produce local HTML report.
+    echo "Producing HTML Report locally."
     grcov ./target/debug/ -s src -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
     open target/debug/coverage/index.html
 elif [ $arg1 = "codecov" ]; then
-    # Produce lcov report and upload it to codecov.io.
+    echo "Producing lcov report and uploading it to codecov.io"
     zip -0 ccov.zip `find . \( -name "exacl*.gc*" \) -print`
     grcov ccov.zip -s src -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info
     bash <(curl -s https://codecov.io/bash) -f lcov.info
