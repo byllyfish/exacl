fb3776c577c6687b2ef132cf7b151a738b028efb
Bill Fisher <william.w.fisher@gmail.com>
2020-10-25 21:31:56 -0700

    membership.h is Apple only.

--- wrapper.h
+++ wrapper.h
@@ -2,5 +2,7 @@
 #include <sys/errno.h>
 #include <sys/acl.h>
 #include <fcntl.h>
-#include <membership.h>
+#if defined(__APPLE__)
+# include <membership.h>
+#endif  // defined(__APPLE__)
 #include <unistd.h>
