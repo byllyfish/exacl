187de2deba85226b84a01d5f63ee104dcc4e17d5
Bill Fisher <william.w.fisher@gmail.com>
2020-10-27 19:49:50 -0700

    Add function comments.

--- src/lib.rs
+++ src/lib.rs
@@ -119,12 +119,13 @@ impl Acl {
         Ok(entries)
     }
 
-    // Construct ACL from platform-dependent textual description.
+    /// Construct ACL from platform-dependent textual description.
     pub fn from_platform_text(text: &str) -> io::Result<Acl> {
         let acl_p = xacl_from_text(text)?;
         Ok(Acl(acl_p))
     }
 
+    /// Return platform-dependent textual description.
     pub fn to_platform_text(&self) -> String {
         xacl_to_text(self.0)
     }
