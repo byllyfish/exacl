2ad844eb3b9b0ec28e0901a7a7cbd4b5b83040d4
Bill Fisher <william.w.fisher@gmail.com>
2020-11-06 20:56:43 -0700

    Return exit status on any failed test.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -14,8 +14,19 @@ if [ ! -f ../target/debug/exacl ]; then
     exit 1
 fi
 
+exit_status=0
+
 for t in testsuite*_all.sh testsuite*_${OS}.sh; do
     # Before running test, print name of file underlined with = signs.
     printf "\n%s\n%s\n" "$t" `printf '=%.0s' $(seq 1 ${#t})`
+    # Run the test.
     ./$t
+    status=$?;
+
+    # Track if any test returns a non-zero exit status.
+    if [ $status -ne 0 ]; then
+        exit_status=$status
+    fi
 done
+
+exit $exit_status
