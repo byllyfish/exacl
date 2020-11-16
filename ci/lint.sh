b3f68e3715f42cff9e07b275044f9c3922a23b0c
Bill Fisher <william.w.fisher@gmail.com>
2020-11-16 14:20:00 -0700

    Log clippy version.

--- ci/lint.sh
+++ ci/lint.sh
@@ -12,10 +12,13 @@ for name in $IGNORE; do
     allow="$allow -A clippy::$name"
 done
 
+# Check rust code with clippy.
 rustup component add clippy
+cargo clippy --version
 cargo clean
 cargo clippy --all-targets --all-features -- -D clippy::all -W clippy::pedantic -W clippy::cargo $allow
 
+# Check bash scripts with shellcheck.
 shellcheck --version
 export SHELLCHECK_OPTS="-e SC1091 -e SC2006 -e SC2016 -e SC2018 -e SC2019 -e SC2086"
 shellcheck ci/*.sh tests/*.sh
