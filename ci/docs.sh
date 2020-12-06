1a933dbd11e2a8e8be23757578cebb956707f043
Bill Fisher <william.w.fisher@gmail.com>
2020-12-06 14:50:48 -0700

    Add index.html file.

--- ci/docs.sh
+++ ci/docs.sh
@@ -17,6 +17,11 @@ if [ "$arg1" = "open" ]; then
     cargo +nightly doc --no-deps --open
 else
     cargo +nightly doc --no-deps
+
+    # Add an index.html file that redirects to our main page.
+    if [ ! -f "target/doc/index.html" ]; then
+        echo '<meta http-equiv=refresh content=0;url=exacl/index.html>' > "target/doc/index.html"
+    fi
 fi
 
 exit 0
