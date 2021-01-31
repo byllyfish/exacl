e728334f9e3b8c41c7330b8a548f73bd5744a265
Bill Fisher <william.w.fisher@gmail.com>
2021-01-31 13:53:47 -0700

    Add Acl::is_nfs4 method.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -8,12 +8,13 @@
 //!    `xacl_init`      - create a new empty ACL
 //!    `xacl_free`      - destroy ACL
 //!    `xacl_foreach`   - apply a function to each entry in an ACL
-//!    `xacl_is_empty`  - test if an ACL is empty
+//!    `xacl_is_empty`  - return true if an ACL is empty
 //!    `xacl_is_posix`  - return true if ACL has Posix.1e semantics.
 //!    `xacl_add_entry` - append new entry to an ACL
-//!    `xacl_get_entry` - retrieve contents of specified ACL entry
+//!    `xacl_get_entry` - retrieve contents from an ACL entry
 //!    `xacl_get_file`  - get ACL from file path
 //!    `xacl_set_file`  - set ACL for file path
+//!    `xacl_is_nfs4`   - return true if file path uses NFSv4 ACL on `FreeBSD`
 
 mod util_common;
 
@@ -32,7 +33,7 @@ pub use crate::sys::{acl_entry_t, acl_t};
 #[cfg(target_os = "freebsd")]
 pub use util_freebsd::{
     xacl_add_entry, xacl_foreach, xacl_free, xacl_get_entry, xacl_get_file, xacl_init,
-    xacl_is_empty, xacl_is_posix, xacl_set_file,
+    xacl_is_empty, xacl_is_nfs4, xacl_is_posix, xacl_set_file,
 };
 
 #[cfg(target_os = "linux")]
