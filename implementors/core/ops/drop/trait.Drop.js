5c2d39f530b512aa1b92a4b3fac7e64ef84c9795
byllyfish <byllyfish@users.noreply.github.com>
2021-02-03 18:13:00 +0000

    deploy: 24bf8884a0f793a3b1d322aa519bf3a4cd42e6f3

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["exacl"] = [{"text":"impl Drop for Acl","synthetic":false,"types":[]}];
+implementors["exacl"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"exacl/struct.Acl.html\" title=\"struct exacl::Acl\">Acl</a>","synthetic":false,"types":["exacl::acl::Acl"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
