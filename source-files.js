f17f7a7314df0bec364ad0669e1da3e353194f11
byllyfish <byllyfish@users.noreply.github.com>
2022-08-14 00:36:44 +0000

    deploy: 87a3ad283a3edef8970c55e5c3c770bff8886ff7

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["exacl"] = {"name":"","dirs":[{"name":"format","files":["format_no_serde.rs","mod.rs"]},{"name":"util","files":["mod.rs","util_common.rs","util_linux.rs"]}],"files":["acl.rs","aclentry.rs","bindings.rs","bititer.rs","failx.rs","flag.rs","lib.rs","perm.rs","qualifier.rs","sys.rs","unix.rs"]};
+var sourcesIndex = JSON.parse('{\
+"exacl":["",[["format",[],["format_no_serde.rs","mod.rs"]],["util",[],["mod.rs","util_common.rs","util_linux.rs"]]],["acl.rs","aclentry.rs","bindings.rs","bititer.rs","failx.rs","flag.rs","lib.rs","perm.rs","qualifier.rs","sys.rs","unix.rs"]]\
+}');
 createSourceSidebar();
